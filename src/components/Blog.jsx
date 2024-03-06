23a961a12bd8db6c891c0ef0733122a0c604ee91
juaxE <juho.kirvesmaki@helsinki.fi>
2024-03-06 15:02:04 +0200

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visibleDetails, setVisibleDetails] = useState(false)
+
+  const showWhenVisible = { display: visibleDetails ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisibleDetails(!visibleDetails)
+  }
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <p>{blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{visibleDetails ? 'hide' : 'view'}</button>
+      </p>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>{blog.likes}
+          <button onClick={() => console.log('liked')}>like</button>
+        </p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
